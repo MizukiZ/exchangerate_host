fcbdf7d26d7cec3a3d74a5d6ed230822f488890a
Mizuki Zenta <mizukisb@gmail.com>
2021-07-24 22:21:44 +1000

    bundle gem

---
+++ lib/exchangerate_host.rb
@@ -0,0 +1,6 @@
+require "exchangerate_host/version"
+
+module ExchangerateHost
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/exchangerate_host/version.rb
@@ -0,0 +1,3 @@
+module ExchangerateHost
+  VERSION = "0.1.0"
+end

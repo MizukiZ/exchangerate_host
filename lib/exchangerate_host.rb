dfa97c515e3334d783a548ac86b175feeae19301
Mizuki Zenta <mizukisb@gmail.com>
2021-07-26 20:30:48 +1000

    create method latest_rates

--- lib/exchangerate_host.rb
+++ lib/exchangerate_host.rb
@@ -1,6 +1,42 @@
 require "exchangerate_host/version"
+require 'httparty'
 
 module ExchangerateHost
-  class Error < StandardError; end
-  # Your code goes here...
+  include HTTParty
+  base_uri 'https://api.exchangerate.host'
+
+  class << self
+    attr_accessor :default_base, :default_symbols, :default_places
+
+    def latest_rates(options)
+      p query_options(options)
+      res = get(
+        '/latest',
+        { query: query_options(options) }
+      )
+      JSON.parse(res.body)['rates']
+    end
+
+    def currencies
+      # return supoprted currency. format [{description: 'Japanese yen', code, 'JPY'}]
+      @currencies ||= JSON.parse(get('/symbols').body)['symbols'].values
+    end
+
+    private
+
+    def query_options(options)
+      symbols = options[:symbols] || default_symbols
+      {
+        base: options[:base] || default_base,
+        symbols: to_upcase_csv(symbols),
+        places: options[:places] || default_places
+      }
+    end
+
+    def to_upcase_csv(array)
+      return '' unless array
+
+      array.map(&:upcase).join(',')
+    end
+  end
 end

ba8d1a98932146cf203b98bf11a2a77ef630b054
mizuki <mizukisb@gmail.com>
2021-07-30 22:34:41 +1000

    fix supported symbols rspec

--- spec/supported_symbols_spec.rb
+++ spec/supported_symbols_spec.rb
@@ -6,12 +6,5 @@ RSpec.describe Endpoints::SupportedSymbols do
         expect { Endpoints::SupportedSymbols.query_options(invalid_option) }.to raise_error(RuntimeError)
       end
     end
-
-    context 'when invalid symbols option value is passed' do
-      invalid_base_value = { symbols: ['ZZZ'] }
-      it 'raises runtime error' do
-        expect { Endpoints::SupportedSymbols.query_options(invalid_base_value) }.to raise_error(RuntimeError)
-      end
-    end
   end
 end

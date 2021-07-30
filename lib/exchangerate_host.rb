d67f75e88e535f07e195e3dc23c8c3c3035dadb5
mizuki <mizukisb@gmail.com>
2021-07-30 14:58:16 +1000

    separate configurations file

--- lib/exchangerate_host.rb
+++ lib/exchangerate_host.rb
@@ -1,15 +1,28 @@
+require 'exchangerate_host/configurations'
 require 'exchangerate_host/query_options'
 require 'exchangerate_host/version'
 require 'httparty'
 
 module ExchangerateHost
+  extend QueryOptions
   include HTTParty
 
-  extend QueryOptions
-
   base_uri 'https://api.exchangerate.host'
-
   class << self
+    attr_accessor :configurations
+
+    def configurations
+      @configurations ||= Configurations.new
+    end
+
+    def configure
+      yield(configurations)
+    end
+
+    def reset_configurations
+      @configurations = Configurations.new
+    end
+
     def latest_rates(options = {})
       res = get(
         '/latest',
--- spec/exchangerate_host_spec.rb
+++ spec/exchangerate_host_spec.rb
@@ -2,4 +2,40 @@ RSpec.describe ExchangerateHost do
   it "has a version number" do
     expect(ExchangerateHost::VERSION).not_to be nil
   end
+
+  describe '#configurations' do
+    it 'returns Configurations' do
+      expect(ExchangerateHost.configurations).to be_a(Configurations)
+    end
+  end
+
+  describe '#configure' do
+    before(:all) do
+      ExchangerateHost.configure do |config|
+        config.base = 'USD'
+        config.symbols = ['AUD', 'JPY']
+        config.amount = 100
+      end
+    end
+
+    it 'sets configurations' do
+      expect(ExchangerateHost.configurations.base).to eq('USD')
+      expect(ExchangerateHost.configurations.symbols).to eq(['AUD', 'JPY'])
+      expect(ExchangerateHost.configurations.amount).to eq(100)
+    end
+  end
+
+  describe '#rest_configurations' do
+    before(:all) do
+      ExchangerateHost.configure do |config|
+        config.base = 'USD'
+      end
+    end
+
+    it 'resets configuration values' do
+      ExchangerateHost.reset_configurations
+      expect(ExchangerateHost.configurations.base).to be_nil
+    end
+  end
+
 end
